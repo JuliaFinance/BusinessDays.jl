1b4aaf68fb93582c9d2f9e0e88bf603cf7b609d6
Felipe Noronha <felipenoris@gmail.com>
2015-07-11 00:05:07 -0300

    removing unused tests

--- test/tests-with-RQuantLib.r
+++ test/tests-with-RQuantLib.r
@@ -5,23 +5,8 @@ from <- as.Date("2015-06-29")
 to <- as.Date("2100-12-20")
 
 microbenchmark ( businessDaysBetween("Brazil", from, to) )
+
 from_vect <- rep(from, 1000000)
 to_vect <- rep(to, 1000000)
 
 microbenchmark( businessDaysBetween("Brazil", from_vect, to_vect), times=1)
-
-
-from_vect <- rep(from, 100)
-to_vect <- rep(to, 100)
-
-microbenchmark ( businessDaysBetween("Brazil", from_vect, to_vect) )
-
-from_vect <- rep(from, 1000)
-to_vect <- rep(to, 1000)
-
-microbenchmark ( businessDaysBetween("Brazil", from_vect, to_vect) )
-
-from_vect <- rep(from, 1000000)
-to_vect <- rep(to, 1000000)
-
-businessDaysBetween("Brazil", from_vect, to_vect)
