16cb26d6ea2e6d20d650bed575f939cf40050fbc
Felipe Noronha <felipenoris@gmail.com>
2015-12-27 15:45:16 -0300

    remove CompositeHolidayCalendar hash function

--- src/composite.jl
+++ src/composite.jl
@@ -5,8 +5,6 @@ type CompositeHolidayCalendar <: HolidayCalendar
 	calendars::Vector{HolidayCalendar}
 end
 
-hash(hc::CompositeHolidayCalendar) = sum(hash(hc.calendars))
-
 function isholiday(hc::CompositeHolidayCalendar, dt::Date)
 	for c in hc.calendars
 		if isholiday(c, dt)
