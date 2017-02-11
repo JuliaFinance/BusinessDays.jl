5c37f5045d69643396cc5c6efa8270c048b83682
Felipe Noronha <felipenoris@gmail.com>
2017-02-11 00:59:49 -0200

    fix 0.6 depwarns

--- src/types.jl
+++ src/types.jl
@@ -29,15 +29,13 @@ function symtocalendar(sym::Symbol)
     end
     return result
 end
-@vectorize_1arg Symbol symtocalendar
 
 strtocalendar(str::AbstractString) = symtocalendar(Symbol(str))
-@vectorize_1arg AbstractString strtocalendar
 
 Base.convert(::Type{HolidayCalendar}, sym::Symbol) = symtocalendar(sym)
 Base.convert(::Type{HolidayCalendar}, str::AbstractString) = strtocalendar(str)
-Base.convert{T<:HolidayCalendar}(::Type{Array{T,1}}, syms::Array{Symbol, 1}) = symtocalendar(syms)
-Base.convert{T<:HolidayCalendar, S<:AbstractString}(::Type{Array{T,1}}, strs::Array{S, 1}) = strtocalendar(strs)
+Base.convert{T<:HolidayCalendar}(::Type{Vector{T}}, syms::Vector{Symbol}) = symtocalendar.(syms)
+Base.convert{T<:HolidayCalendar, S<:AbstractString}(::Type{Array{T,1}}, strs::Array{S, 1}) = strtocalendar.(strs)
 
 """
     isholiday(calendar, dt)
