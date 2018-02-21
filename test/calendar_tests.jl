497a6dc1d1dfcf1f973fc1f317d370a01ec7763e
Felipe Noronha <felipenoris@gmail.com>
2018-02-20 23:38:56 -0300

    tests for bdayscount

--- test/calendar_tests.jl
+++ test/calendar_tests.jl
@@ -29,12 +29,16 @@ if !usecache
     isbday(hc_brazil, Date(3000,2,1))
     bdays(hc_brazil, Date(1600,2,1), Date(1600, 2, 5))
     bdays(hc_brazil, Date(3000,2,1), Date(3000, 2, 5))
+    bdayscount(hc_brazil, Date(1600,2,1), Date(1600, 2, 5))
+    bdayscount(hc_brazil, Date(3000,2,1), Date(3000, 2, 5))
 else
     #this should not work
     @test_throws AssertionError isbday(hc_brazil, Date(1600,2,1))
     @test_throws AssertionError isbday(hc_brazil, Date(3000,2,1))
     @test_throws AssertionError bdays(hc_brazil, Date(1600,2,1), Date(1600, 2, 5))
     @test_throws AssertionError bdays(hc_brazil, Date(3000,2,1), Date(3000, 2, 5))
+    @test_throws AssertionError bdayscount(hc_brazil, Date(1600,2,1), Date(1600, 2, 5))
+    @test_throws AssertionError bdayscount(hc_brazil, Date(3000,2,1), Date(3000, 2, 5))
 end
 
 @test_throws ErrorException isbday(:UnknownCalendar, Date(2016,1,1))
@@ -87,7 +91,8 @@ end
 @test isbday(bd.NullHolidayCalendar(), Date(2016,9,25)) == true
 @test isbday(:NullHolidayCalendar, Date(2016,9,25)) == true
 @test isbday("NullHolidayCalendar", Date(2016,9,25)) == true
-@test bdays(:NullHolidayCalendar, Date(2016,9,25), Date(2016,9,28)) == Day(3)
+@test bdayscount(:NullHolidayCalendar, Date(2016,9,25), Date(2016,9,28)) == 3
+@test bdays(:NullHolidayCalendar, Date(2016,9,25), Date(2016,9,28)) == Dates.Day(3)
 
 @test isholiday(bd.WeekendsOnly(), Date(2016,9,25)) == false
 @test isholiday(:WeekendsOnly, Date(2016,9,25)) == false
@@ -95,7 +100,8 @@ end
 @test isbday(bd.WeekendsOnly(), Date(2016,9,25)) == false
 @test isbday(:WeekendsOnly, Date(2016,9,25)) == false
 @test isbday("WeekendsOnly", Date(2016,9,25)) == false
-@test bdays(:WeekendsOnly, Date(2016,9,25), Date(2016,9,28)) == Day(2)
+@test bdayscount(:WeekendsOnly, Date(2016,9,25), Date(2016,9,28)) == 2
+@test bdays(:WeekendsOnly, Date(2016,9,25), Date(2016,9,28)) == Dates.Day(2)
 
 
 # Brazil HolidayCalendar tests
@@ -496,8 +502,8 @@ end
 @test advancebdays("Brazil", Date(2013, 02, 06), 3) == Date(2013, 02, 13) # after carnaval wednesday
 @test advancebdays("Brazil", Date(2013, 02, 06), 4) == Date(2013, 02, 14) # after carnaval thursday
 
-@test bdays(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 06)) == Day(0)
-@test bdays(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 07)) == Day(1)
+@test bdays(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 06)) == Dates.Day(0)
+@test bdays(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 07)) == Dates.Day(1)
 @test bdays(hc_brazil, Date(2013, 02, 07), Date(2013, 02, 06)).value == -1
 @test bdays(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 08)).value == 2
 @test bdays(hc_brazil, Date(2013, 02, 08), Date(2013, 02, 06)).value == -2
@@ -508,6 +514,19 @@ end
 @test bdays(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 13)).value == 3
 @test bdays(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 14)).value == 4
 @test bdays(hc_brazil, Date(2013, 02, 14), Date(2013, 02, 06)).value == -4
+
+@test bdayscount(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 06)) == 0
+@test bdayscount(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 07)) == 1
+@test bdayscount(hc_brazil, Date(2013, 02, 07), Date(2013, 02, 06)) == -1
+@test bdayscount(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 08)) == 2
+@test bdayscount(hc_brazil, Date(2013, 02, 08), Date(2013, 02, 06)) == -2
+@test bdayscount(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 09)) == 3
+@test bdayscount(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 10)) == 3
+@test bdayscount(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 11)) == 3
+@test bdayscount(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 12)) == 3
+@test bdayscount(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 13)) == 3
+@test bdayscount(hc_brazil, Date(2013, 02, 06), Date(2013, 02, 14)) == 4
+@test bdayscount(hc_brazil, Date(2013, 02, 14), Date(2013, 02, 06)) == -4
 
 
 # Canada
@@ -525,7 +544,7 @@ tsxdates16 = [
     Date(2016, 12, 27)
 ]
 
-alldates16 = collect(Date(2016,1,1):Day(1):Date(2016, 12,31))
+alldates16 = collect(Date(2016,1,1):Dates.Day(1):Date(2016, 12,31))
 for i in alldates16
     if contains(==, tsxdates16, i)
         @test isholiday(hc_canadatsx, i) == true
@@ -546,7 +565,7 @@ tsxdates17 = [
     Date(2017, 12, 25),
     Date(2017, 12, 26)
 ]
-alldates17 = collect(Date(2017,1,1):Day(1):Date(2017, 12,31))
+alldates17 = collect(Date(2017,1,1):Dates.Day(1):Date(2017, 12,31))
 
 for i in alldates17
     if contains(==, tsxdates17, i)
@@ -598,9 +617,9 @@ asxdates18 = Set([
 ])
 for dt in Date(2018,1,1):Day(1):Date(2018,12,31)
     if in(dt, asxdates18)
-        dt != boxingday && @test isholiday(hc_australiaasx, dt-Day(1)) == false
+        dt != boxingday && @test isholiday(hc_australiaasx, dt-Dates.Day(1)) == false
         @test isholiday(hc_australiaasx, dt) == true
-        dt != christmasday && @test isholiday(hc_australiaasx, dt+Day(1)) == false
+        dt != christmasday && @test isholiday(hc_australiaasx, dt+Dates.Day(1)) == false
     else
         @test isholiday(hc_australiaasx, dt) == false
     end
@@ -626,7 +645,7 @@ actdates18 = Set([
     christmasday,
     boxingday
 ])
-for dt in Date(2018,1,1):Day(1):Date(2018,12,31)
+for dt in Date(2018,1,1):Dates.Day(1):Date(2018,12,31)
     if in(dt, actdates18)
         @test isholiday(hc_australiaact, dt) == true
     else
@@ -649,7 +668,7 @@ nswdates18 = Set([
     christmasday,
     boxingday
 ])
-for dt in Date(2018,1,1):Day(1):Date(2018,12,31)
+for dt in Date(2018,1,1):Dates.Day(1):Date(2018,12,31)
     if in(dt, nswdates18)
         @test isholiday(hc_australiansw, dt) == true
     else
@@ -671,7 +690,7 @@ ntdates18 = Set([
     christmasday,
     boxingday
 ])
-for dt in Date(2018,1,1):Day(1):Date(2018,12,31)
+for dt in Date(2018,1,1):Dates.Day(1):Date(2018,12,31)
     if in(dt, ntdates18)
         @test isholiday(hc_australiant, dt) == true
     else
@@ -694,7 +713,7 @@ qlddates18 = Set([
     christmasday,
     boxingday
 ])
-for dt in Date(2018,1,1):Day(1):Date(2018,12,31)
+for dt in Date(2018,1,1):Dates.Day(1):Date(2018,12,31)
     if in(dt, qlddates18)
         @test isholiday(hc_australiaqld, dt) == true
     else
@@ -717,7 +736,7 @@ sadates18 = Set([
     christmasday,
     boxingday
 ])
-for dt in Date(2018,1,1):Day(1):Date(2018,12,31)
+for dt in Date(2018,1,1):Dates.Day(1):Date(2018,12,31)
     if in(dt, sadates18)
         @test isholiday(hc_australiasa, dt) == true
     else
@@ -760,7 +779,7 @@ wadates18 = Set([
     christmasday,
     boxingday
 ])
-for dt in Date(2018,1,1):Day(1):Date(2018,12,31)
+for dt in Date(2018,1,1):Dates.Day(1):Date(2018,12,31)
     if in(dt, wadates18)
         @test isholiday(hc_australiawa, dt) == true
     else
@@ -786,7 +805,7 @@ vicdates18 = Set([
     christmasday,
     boxingday
 ])
-for dt in Date(2018,1,1):Day(1):Date(2018,12,31)
+for dt in Date(2018,1,1):Dates.Day(1):Date(2018,12,31)
     if in(dt, vicdates18)
         @test isholiday(hc_australiavic, dt) == true
     else
@@ -807,7 +826,8 @@ d0 = Date(2015, 06, 29) ; d2 = Date(2100, 12, 20)
 # Tests for Composite Calendar
 @test isholiday(hc_composite_BR_USA, Date(2012,9,3)) # US Labor Day
 @test isholiday(hc_composite_BR_USA, Date(2012,9,7)) # BR Independence Day
-@test bdays(hc_composite_BR_USA, Date(2012,8,31), Date(2012,9,10)) == Day(4) # 3/sep labor day US, 7/sep Indep day BR
+@test bdayscount(hc_composite_BR_USA, Date(2012,8,31), Date(2012,9,10)) == 4 # 3/sep labor day US, 7/sep Indep day BR
+@test bdays(hc_composite_BR_USA, Date(2012,8,31), Date(2012,9,10)) == Dates.Day(4) # 3/sep labor day US, 7/sep Indep day BR
 
 println("Timing composite calendar bdays calculation")
 @time bdays(hc_composite_BR_USA, Date(2012,8,31), Date(2012,9,10))
@@ -869,18 +889,21 @@ d2001 = collect(Date(2001,01,01):Date(2001,01,15))
 @test tobday("Brazil", d2001; forward=false) == [ Date(2000,12,29), Date(2001,01,02), Date(2001,01,03), Date(2001,01,04), Date(2001,01,05), Date(2001,01,05), Date(2001,01,05), Date(2001,01,08), Date(2001,01,09), Date(2001,01,10), Date(2001,01,11), Date(2001,01,12), Date(2001,01,12), Date(2001,01,12), Date(2001,01,15)]
 
 @test bdays([hc_brazil, hc_usa], [Date(2012,8,31), Date(2012,8,31)], [Date(2012,9,10), Date(2012,9,10)]) == [Day(5), Day(5)] # 1/sep labor day US, 7/sep Indep day BR
+@test bdayscount([hc_brazil, hc_usa], [Date(2012,8,31), Date(2012,8,31)], [Date(2012,9,10), Date(2012,9,10)]) == [5, 5] # 1/sep labor day US, 7/sep Indep day BR
 @test isbday([hc_brazil, hc_usa], [Date(2012, 09, 07), Date(2012, 09, 03)]) == [false, false] # 1/sep labor day US, 7/sep Indep day BR
 @test advancebdays(hc_brazil, Date(2015,9,1), [0, 1, 3, 4, 5]) == [Date(2015,9,1),Date(2015,9,2),Date(2015,9,4),Date(2015,9,8),Date(2015,9,9)]
 @test advancebdays(hc_brazil, Date(2015,9,1), 0:5) == [Date(2015,9,1),Date(2015,9,2),Date(2015,9,3),Date(2015,9,4),Date(2015,9,8),Date(2015,9,9)]
 @test listholidays(hc_brazil, Date(2016,1,1), Date(2016,5,30)) == [Date(2016,1,1),Date(2016,2,8),Date(2016,2,9),Date(2016,3,25),Date(2016,4,21),Date(2016,5,1),Date(2016,5,26)]
 
 @test bdays([:Brazil, :USSettlement], [Date(2012,8,31), Date(2012,8,31)], [Date(2012,9,10), Date(2012,9,10)]) == [Day(5), Day(5)] # 1/sep labor day US, 7/sep Indep day BR
+@test bdayscount([:Brazil, :USSettlement], [Date(2012,8,31), Date(2012,8,31)], [Date(2012,9,10), Date(2012,9,10)]) == [5, 5] # 1/sep labor day US, 7/sep Indep day BR
 @test isbday([:Brazil, :USSettlement], [Date(2012, 09, 07), Date(2012, 09, 03)]) == [false, false] # 1/sep labor day US, 7/sep Indep day BR
 @test advancebdays(:Brazil, Date(2015,9,1), [0, 1, 3, 4, 5]) == [Date(2015,9,1),Date(2015,9,2),Date(2015,9,4),Date(2015,9,8),Date(2015,9,9)]
 @test advancebdays(:Brazil, Date(2015,9,1), 0:5) == [Date(2015,9,1),Date(2015,9,2),Date(2015,9,3),Date(2015,9,4),Date(2015,9,8),Date(2015,9,9)]
 @test listholidays(:Brazil, Date(2016,1,1), Date(2016,5,30)) == [Date(2016,1,1),Date(2016,2,8),Date(2016,2,9),Date(2016,3,25),Date(2016,4,21),Date(2016,5,1),Date(2016,5,26)]
 
 @test bdays(["Brazil", "USSettlement"], [Date(2012,8,31), Date(2012,8,31)], [Date(2012,9,10), Date(2012,9,10)]) == [Day(5), Day(5)] # 1/sep labor day US, 7/sep Indep day BR
+@test bdayscount(["Brazil", "USSettlement"], [Date(2012,8,31), Date(2012,8,31)], [Date(2012,9,10), Date(2012,9,10)]) == [5, 5] # 1/sep labor day US, 7/sep Indep day BR
 @test isbday(["Brazil", "USSettlement"], [Date(2012, 09, 07), Date(2012, 09, 03)]) == [false, false] # 1/sep labor day US, 7/sep Indep day BR
 @test advancebdays("Brazil", Date(2015,9,1), [0, 1, 3, 4, 5]) == [Date(2015,9,1),Date(2015,9,2),Date(2015,9,4),Date(2015,9,8),Date(2015,9,9)]
 @test advancebdays("Brazil", Date(2015,9,1), 0:5) == [Date(2015,9,1),Date(2015,9,2),Date(2015,9,3),Date(2015,9,4),Date(2015,9,8),Date(2015,9,9)]
@@ -893,6 +916,7 @@ d2001 = collect(Date(2001,01,01):Date(2001,01,15))
 @test listbdays("Brazil", Date(2016,5,21), Date(2016,5,23)) == [Date(2016,5,23)]
 
 @test bdays(:Brazil, Date(2016,11,1), [Date(2016,11,3), Date(2016,11,7), Date(2016,11,4)]) == [Day(1), Day(3), Day(2)]
+@test bdayscount(:Brazil, Date(2016,11,1), [Date(2016,11,3), Date(2016,11,7), Date(2016,11,4)]) == [1, 3, 2]
 
 # first/last businessdayofmonth
 @test firstbdayofmonth(:Brazil, Date(2017,12,10)) == Date(2017,12,1)
