e278d6dec1bebf69d8057d523817c9aa72ce5d28
Felipe Noronha <felipenoris@gmail.com>
2020-03-07 17:34:32 -0300

    fix website link

--- src/BusinessDays.jl
+++ src/BusinessDays.jl
@@ -2,7 +2,7 @@
 """
 A highly optimized Business Days calculator written in Julia language. Also known as Working Days calculator.
 
-Website: https://github.com/felipenoris/BusinessDays.jl
+Website: https://github.com/JuliaFinance/BusinessDays.jl
 """
 module BusinessDays
 
